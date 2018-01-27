82eb39ee70fbd6fd174f6354228007ee500dfe47
David Ko <dk577@cornell.edu>
2018-01-27 10:10:31 -0500

    babelpolyfill

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './Home.jsx'
+require('babel-core/register');
+require('babel-polyfill');
 
 ReactDOM.render(
   <Home />,
