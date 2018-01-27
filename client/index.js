2e88ef617693401540cb14d4aa7b57792373d0b0
David Ko <dk577@cornell.edu>
2018-01-27 10:41:26 -0500

    webpack

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './Home.jsx'
-require('babel-core/register');
-require('babel-polyfill');
 
 ReactDOM.render(
   <Home />,
